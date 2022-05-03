4c4c9f0659fb469ff275de5bc2b7d7edeb9a4ebe
Kacper <56606499+KaoticKas@users.noreply.github.com>
2022-05-03 07:03:48 +0100

    Colour masked implemented

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 import numpy
-
 import cv2
-from cv2 import namedWindow
 import cv_bridge
 import rospy
 
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 
 
 class Follower:
@@ -17,36 +16,58 @@ class Follower:
         self.cmd_vel_pub = rospy.Publisher('/mobile_base/commands/velocity', Twist,
                                            queue_size=1)
         self.twist = Twist()
+    def laser_cb(self, lasermsg):
+        if lasermsg.ranges[320] < 1.0:
+            t = Twist()
+            
 
     def image_callback(self, msg):
-        cv2.namedWindow("window", 1)
-        image = self.bridge.imgmsg_to_cv2(msg)
-        #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        #lower_yell = numpy.array([10, 60, 70])
-        #upper_yell = numpy.array([255, 255, 255])
-        #mask = cv2.inRange(hsv, lower_yell, upper_yell)
-        lower_blue = numpy.array([255, 0, 0])
-        upper_blue = numpy.array([255, 0, 0])
-        mask = cv2.inRange(image, lower_blue, upper_blue)
+        image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        
+        lower_yell = numpy.array([20, 100, 100])
+        upper_yell = numpy.array([30, 255, 255])
+        mask_yellow = cv2.inRange(hsv, lower_yell, upper_yell)
 
-        cv2.bitwise_and(image, image, mask=mask)
+        lower_blue = numpy.array([120,50,50])
+        upper_blue = numpy.array([150,255,255])
+        mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+        
+        lower_red = numpy.array([0,60,60])
+        upper_red = numpy.array([15,255,255])
+        mask_red = cv2.inRange(hsv, lower_red, upper_red)
+
+        satthresh = cv2.inRange(hsv,numpy.array((0,150,50)),numpy.array((255,255,255)))
+
+        yellowbit =cv2.bitwise_and(image, image, mask=mask_yellow)
+        bluebit =cv2.bitwise_and(image, image, mask=mask_blue)
+        redbit =cv2.bitwise_and(image, image, mask=mask_red)
         h, w, d = image.shape
         search_top = h
         search_bot = h
         #mask[0:search_top, 0:w] = 0
         #mask[search_bot:h, 0:w] = 0
-        M = cv2.moments(mask)
-        if M['m00'] > 0:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            cv2.circle(image, (cx, cy), 20, (0, 0, 255), -1)
-            err = cx - w/2
+        #M = cv2.moments(mask)
+        #if M['m00'] > 0:
+            #cx = int(M['m10']/M['m00'])
+            #cy = int(M['m01']/M['m00'])
+            #cv2.circle(image, (cx, cy), 20, (0, 0, 255), -1)
+            #err = cx - w/2
             #self.twist.linear.x = 0.2
             #self.twist.angular.z = -float(err) / 100
-            print (self.twist.angular.z)
+            #print (self.twist.angular.z)
 
-            self.cmd_vel_pub.publish(self.twist)
-        cv2.imshow("window", mask)
+            #self.cmd_vel_pub.publish(self.twist)
+        print("hi mark")
+
+        print(cv2.mean(hsv[:, :, 0], mask = satthresh)[0])
+        print(cv2.mean(hsv[:, :, 1], mask = satthresh)[0])
+
+        cv2.imshow("blue", mask_blue)
+        cv2.imshow("red", mask_red)
+        cv2.imshow("yellow", mask_yellow)
+        cv2.imshow("hsv", hsv)
+
         cv2.waitKey(3)
 
 
